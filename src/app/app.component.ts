11c203f3c62115bb79650dfb8e93c77e3a8d691a
rshelar5 <rohan.shelar@icloud.com>
2021-12-14 02:34:15 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'daily';
-  today = window.Date.now.call;
-  goals = ["Daily Changes","Detox","Healthy lifestyle","Mau Time","Kizzi Time","Happda Time","Dota Time","Fixed Income","Growth Funds","Interview Preperation"];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GoalComponent } from './goal/goal.component';
+import { AppTodoComponent } from './components/app-todo/app-todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GoalComponent
+    AppTodoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [
+    AppTodoComponent
+  ]
 })
 export class AppModule { }

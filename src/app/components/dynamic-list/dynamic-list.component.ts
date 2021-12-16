0ec8e615e19b8f5bf88c1c25ada7c2474ac416cc
rshelar5 <82907554+rshelar5@users.noreply.github.com>
2021-12-16 16:11:01 +0530

    first add on

--- src/app/components/dynamic-list/dynamic-list.component.ts
+++ src/app/components/dynamic-list/dynamic-list.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
+
+import { MatPseudoCheckbox } from '@angular/material/core';
 
 @Component({
   selector: 'app-dynamic-list',
   templateUrl: './dynamic-list.component.html',
-  styleUrls: ['./dynamic-list.component.scss']
+  styleUrls: ['./dynamic-list.component.scss'],
 })
-export class DynamicListComponent implements OnInit {
-
-  constructor() { }
-
-
-  ngOnInit(): void {
+export class DynamicListComponent implements OnInit, OnChanges {
+  constructor() {}
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
   }
 
+  currentValue: string | any;
+  addNew(newEntry: any) {
+    this.dataSource.unshift(newEntry.value);
+    newEntry.value = '';
+  }
+  dataSource = ['aaa', 'bbb', 'ccc'];
+  ngOnInit(): void {}
 }
